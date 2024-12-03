e80b7adc78d8562bc3ec32dcfe84a753dcb5b57f
Mounika <mounikasaiaarepu09@gmail.com>
2024-12-03 18:55:12 +0530

    some-message

--- 15.current-script.sh
+++ 15.current-script.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 COURSE="Devops from current script"
     echo " Before caling a script , course : $COURSE "
+    echo " process Id of current script is : $$ "
     #./16.other-script.sh
     echo " After calling a script , course : $COURSE "
  
