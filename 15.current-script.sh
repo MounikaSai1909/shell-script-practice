be458c5412deea68521b1f595abbd1f3f0639e5f
Mounika <mounikasaiaarepu09@gmail.com>
2024-12-03 18:50:08 +0530

    some-message

---
+++ 15.current-script.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+COURSE="Devops from current script"
+    echo " Before caling a script , course : $COURSE "
+    #./16.other-script.sh
+    echo " After calling a script , course : $COURSE "
+ 
